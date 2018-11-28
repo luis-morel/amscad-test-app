875ddf20f46abd82be9b10837eb33c05a96326fe
LMorel77 <lmorel77@aol.com>
2018-11-28 00:37:01 -0500

    Directory cleanup

--- client/src/components/PostForm2/index.js
+++
@@ -1,1 +0,0 @@
-export { default } from "./PostForm2.js";
--- client/src/components/Twitter/index.js
+++
@@ -1,5 +0,0 @@
-export { default } from "./Twitter.js";
-export { default as facebook } from "./fb.png";
-export { default as github } from "./gith.png";
-export { default as instagram } from "./insta.png";
-export { default as twitter } from "./twit.png";
